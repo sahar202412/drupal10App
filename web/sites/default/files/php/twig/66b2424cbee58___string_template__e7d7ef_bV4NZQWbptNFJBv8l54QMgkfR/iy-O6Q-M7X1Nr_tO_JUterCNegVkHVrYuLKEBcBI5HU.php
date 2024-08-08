8ee29276261ede787da7b65d00de368ff142accf
Sahar Moussa <smoussa@192.168.1.80>
2024-08-08 13:58:16 +0100

    Drupal translation  modules

---
+++ web/sites/default/files/php/twig/66b2424cbee58___string_template__e7d7ef_bV4NZQWbptNFJBv8l54QMgkfR/iy-O6Q-M7X1Nr_tO_JUterCNegVkHVrYuLKEBcBI5HU.php
@@ -0,0 +1,92 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__e7d7effdefe3df1a677b7e5b614d2fea */
+class __TwigTemplate_516331bee7c1abdd9bdd55df33dd0a64 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension(SandboxExtension::class);
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        yield "destination=/en/admin/structure/views/view/reviews/preview/page_1%3F_wrapper_format%3Ddrupal_ajax";
+        return; yield '';
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "__string_template__e7d7effdefe3df1a677b7e5b614d2fea";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__e7d7effdefe3df1a677b7e5b614d2fea", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/66b2424cbee58_views-view-row-rss.html.t_NQ8DvORn9a5TDe9qNtapcuv9n/wOcuHg3dDFZS1xWsgm6C5DK2pv3ovl5aCVoGyrxOpPQ.php
@@ -0,0 +1,144 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/claro/templates/classy/views/views-view-row-rss.html.twig */
+class __TwigTemplate_2a1c0b3c9c643ae9f4242208ccfe2401 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension(SandboxExtension::class);
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 18
+        yield "<item>
+  <title>";
+        // line 19
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 19, $this->source), "html", null, true);
+        yield "</title>
+  <link>";
+        // line 20
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["link"] ?? null), 20, $this->source), "html", null, true);
+        yield "</link>
+  <description>";
+        // line 21
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null), 21, $this->source), "html", null, true);
+        yield "</description>
+  ";
+        // line 22
+        $context['_parent'] = $context;
+        $context['_seq'] = CoreExtension::ensureTraversable(($context["item_elements"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+            // line 23
+            yield "<";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, $context["item"], "key", [], "any", false, false, true, 23), 23, $this->source), "html", null, true);
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 23), 23, $this->source), "html", null, true);
+            // line 24
+            if (CoreExtension::getAttribute($this->env, $this->source, $context["item"], "value", [], "any", false, false, true, 24)) {
+                // line 25
+                yield ">";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, $context["item"], "value", [], "any", false, false, true, 25), 25, $this->source), "html", null, true);
+                yield "</";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, $context["item"], "key", [], "any", false, false, true, 25), 25, $this->source), "html", null, true);
+                yield ">
+    ";
+            } else {
+                // line 27
+                yield " />
+    ";
+            }
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 30
+        yield "</item>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["title", "link", "description", "item_elements"]);        return; yield '';
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "core/themes/claro/templates/classy/views/views-view-row-rss.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  81 => 30,  73 => 27,  65 => 25,  63 => 24,  59 => 23,  55 => 22,  51 => 21,  47 => 20,  43 => 19,  40 => 18,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/claro/templates/classy/views/views-view-row-rss.html.twig", "/var/www/html/web/core/themes/claro/templates/classy/views/views-view-row-rss.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 22, "if" => 24);
+        static $filters = array("escape" => 19);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'if'],
+                ['escape'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
